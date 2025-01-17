f3df0e35f5302d19cafcdf5f8b2b88e22d460311
AlexandrNeverov <nev.al.vic1@gmail.com>
2025-01-17 14:38:50 -0500

    5

--- main.tf
+++ main.tf
@@ -20,6 +20,16 @@ terraform {
     }
 }
 
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
+
 provider "aws" {
   region     = "us-east-1"          
   access_key = file ("/home/ubuntu/terraform/projects/s1.tfvars")      
