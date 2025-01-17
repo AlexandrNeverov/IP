79f0b89f80375fea0dc51a333f2b5ffda797abfa
AlexandrNeverov <nev.al.vic1@gmail.com>
2025-01-17 14:49:46 -0500

    5

--- main.tf
+++ main.tf
@@ -20,6 +20,16 @@ terraform {
     }
 }
 
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
+
 provider "aws" {
   region     = "us-east-1"          
   access_key = var.aws_access_key    
